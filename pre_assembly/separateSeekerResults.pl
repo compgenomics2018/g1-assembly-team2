2f45a4e81dbe5425cabcc0372a621476d6916ad4
Saldana_Farias_Beatriz_E_bsaldana3 <beatrizsaldana@gatech.edu>
2018-03-05 00:17:57 -0500

    added separateSeekerResults.pl script

---
+++ pre_assembly/separateSeekerResults.pl
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+
+my $list = $ARGV[0]; #txt file with a list of the file names
+my $outfile = $ARGV[1]; #file name of the output
+my @filenames; #array containing the list of file names (one file per array element)
+
+open(OUT, "+>", $outfile); #open the output file
+
+#open the list of file names
+open(FILELIST, $list) or die "Could not open $list\n";
+while (<FILELIST>)
+{
+    chomp $_; #remove end line symbol
+    $_ =~ s/\r//g; #remove carriage return
+
+    push @filenames, $_; #name array of filenames (one file per array element)
+}
+
+my $number_of_files = scalar @filenames; #store the number of files in list
+
+#open every file, one by one
+for (my $i; $i <= $number_of_files; $i++)
+{
+    my $sample = $filenames[$i]; #store the sample name
+    my $percentage;
+    my $relatedList;
+    my $reference_species;
+
+    open(FILE, $filenames[$i]) or die "Could not open $filenames[$i]\n";
+    while (<FILE>)
+    {
+        chomp $_; #remove end line symbol
+        $_ =~ s/\r//g; #remove carriage return just in case
+        my @line = split (/\s/,$_); #create array of percentage \t 'RELATED' \t list of related species
+
+        $percentage = chomp $line[0]; #keep the percentage (remove '%' symbol)
+        $relatedList = $line[2]; #keep the lsit of related species
+    }
+
+    #if the percentage is above 90% then store the first 'related' species
+    if ($percentage > 90)
+    {
+        my @reference = split (/,/,$relatedList); #split comma separated list
+        $reference_species = $reference[0]; #keep the first element of the list, aka the most related species
+    }
+
+    print OUT "$sample\t$reference_species\n"; #print out the sample name and the reference species it is most related to
+}
