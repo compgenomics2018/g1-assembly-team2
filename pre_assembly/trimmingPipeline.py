e173ccc0d3c315e8a7b2deef0ce6c466b096e9d3
William Harvey <williamharvey@ipsec-143-215-28-120.vpn.gatech.edu>
2018-03-04 22:51:35 -0500

    Added trimming script and reference assembly scripts

---
+++ pre_assembly/trimmingPipeline.py
@@ -0,0 +1,18 @@
+import os
+
+
+f = open("ReadList.txt", "r")
+ReadList = []
+for line in f:
+	ReadList.append(line.rstrip())
+f.close()
+
+l = len(ReadList)
+
+for i in range (0, l, 2)
+	os.system("trim_galore --illumina -q 27 --clip_R1 20 --clip_R2 20 --three_prime_clip_R1 5 --three_prime_clip_R2 5 --paired /projects/data/sra_1_fastq/"+str(ReadList[i])+" /projects/data/sra_1_fastq/"+str(ReadList[i+1])+" -o /projects/data/temp")
+	x = ReadList[i][0:10]
+	os.system("sickle pe -q 27 -f /projects/data/temp/"+str(x)+"_1_val_1.fq -r /projects/data/temp/"+str(x)+"_2_val_2.fq -t sanger -o /projects/data/sra_1_fastq_trimmedFINAL/"+str(x)+"_1_27.fastq -p /projects/data/sra_1_fastq_trimmedFINAL/"+str(x)+"_2_27.fastq -s /projects/data/sra_1_fastq_trimmedSINGLE/"+str(x)+"_singles_27.fastq")
+
+os.system("mv ../temp/*trimming* ../sra_1_fastq_trimmedFINAL/")
+os.system("rm ../temp/*val*")
